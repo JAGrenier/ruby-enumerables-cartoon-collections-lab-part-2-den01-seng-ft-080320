9b29da7218bc994ae6ee73d4d03bd330a6cd588f
Jennifer Grenier <jenniferagrenier@gmail.com>
2020-06-24 19:33:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,18 @@
 def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
+  new_array=[]
+  index = 0 
+
+  while index < array.length do 
+    new_array << array[index] * array[index]
+    index += 1 
+  end 
+
+  new_array 
 end
+
+square_array([1,2,3,4])
 
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
