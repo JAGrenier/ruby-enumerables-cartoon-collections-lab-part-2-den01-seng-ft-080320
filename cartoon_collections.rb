5764d57c9da75f891493892ad677d7e5e0803622
Jennifer Grenier <jenniferagrenier@gmail.com>
2020-06-24 19:49:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def long_planeteer_calls(planeteer_calls)
   planeteer_calls.find do |element|
     element.length > 4
   end
-end 
+end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
